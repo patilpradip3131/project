02aba6ac134026f9236b9bbefa2e53a08c18504e
patilpradip3131 <124864096+patilpradip3131@users.noreply.github.com>
2023-03-27 01:05:07 +0530

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 
-			  sh "sed -i 's|localhost|database-1.cggvyyskcoln.ap-south-1.rds.amazonaws.com|g' LoginWebApp/login.jsp"
-			  sh "sed -i 's|localhost|database-1.cggvyyskcoln.ap-south-1.rds.amazonaws.com|g' LoginWebApp/userRegistration.jsp"
-		    sh "sed -i 's|username=root|username=admin|g' LoginWebApp/login.jsp"
-			  sh "sed -i 's|password=root|password=admin123456|g' LoginWebApp/login.jsp"
-			  sh "sed -i 's|username=root|username=admin|g' LoginWebApp/userRegistration.jsp"
-			  sh "sed -i 's|password=root|password=admin123456|g' LoginWebApp/userRegistration.jsp"
+  sh "sed -i 's|localhost|database-1.cggvyyskcoln.ap-south-1.rds.amazonaws.com|g' LoginWebApp/login.jsp"
+  sh "sed -i 's|localhost|database-1.cggvyyskcoln.ap-south-1.rds.amazonaws.com|g' LoginWebApp/userRegistration.jsp"
+  sh "sed -i 's|username=root|username=admin|g' LoginWebApp/login.jsp"
+  sh "sed -i 's|password=root|password=admin123456|g' LoginWebApp/login.jsp"
+  sh "sed -i 's|username=root|username=admin|g' LoginWebApp/userRegistration.jsp"
+  sh "sed -i 's|password=root|password=admin123456|g' LoginWebApp/userRegistration.jsp"
+
+ sh "sed -i 's|password=root|password=admin123456|g' LoginWebApp/userRegistration.jsp"
