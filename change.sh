6fe2d9ddd90de8cd7940907dd17e4ecf458f77c5
patilpradip3131 <124864096+patilpradip3131@users.noreply.github.com>
2023-03-27 02:21:36 +0530

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-  sh "sed -i 's|localhost|database-1.cggvyyskcoln.ap-south-1.rds.amazonaws.com|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/login.jsp"
-  sh "sed -i 's|localhost|database-1.cggvyyskcoln.ap-south-1.rds.amazonaws.com|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/userRegistration.jsp"
-  sh "sed -i 's|username=root|username=admin|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/login.jsp"
-  sh "sed -i 's|password=root|password=admin123456|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/login.jsp"
-  sh "sed -i 's|username=root|username=admin|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/userRegistration.jsp"
-  sh "sed -i 's|password=root|password=admin123456|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/userRegistration.jsp"
-  sh "sed -i 's|password=root|password=admin123456|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/userRegistration.jsp"
+  sed -i 's|localhost|database-1.cggvyyskcoln.ap-south-1.rds.amazonaws.com|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/login.jsp
+  sed -i 's|localhost|database-1.cggvyyskcoln.ap-south-1.rds.amazonaws.com|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/userRegistration.jsp
+  sed -i 's|username=root|username=admin|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/login.jsp
+  sed -i 's|password=root|password=admin123456|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/login.jsp
+  sed -i 's|username=root|username=admin|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/userRegistration.jsp
+  sed -i 's|password=root|password=admin123456|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/userRegistration.jsp
+  sed -i 's|password=root|password=admin123456|g' /mnt/server/apache-tomcat-9.0.73/webapps/LoginWebApp/userRegistration.jsp
+
